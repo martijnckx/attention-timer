09164018a796c71e99cbf2560b99313593255262
Martijn Luyckx <martijn.luyckx@student.uhasselt.be>
2017-06-05 18:17:26 +0200

    New boilerplate

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,52 +1,25 @@
-/**
- * System configuration for Angular samples
- * Adjust as necessary for your application needs.
- */
 (function (global) {
   System.config({
-    paths: {
-      // paths serve as alias
-      'npm:': 'node_modules/'
-    },
-    // map tells the System loader where to look for things
     map: {
-      // our app is within the app folder
-      'app': 'app',
-
-      // angular bundles
-      '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
-      '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/router/upgrade': 'npm:@angular/router/bundles/router-upgrade.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
-      '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
-
-      // other libraries
-      'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      '@angular': 'node_modules/@angular',
+      'rxjs': 'node_modules/rxjs'
     },
-    // packages tells the System loader how to load when no filename and/or no extension
+    paths: {
+      'node_modules/@angular/*': 'node_modules/@angular/*/bundles'
+    },
+    meta: {
+      '@angular/*': {'format': 'cjs'}
+    },
     packages: {
-      app: {
-        main: './main.js',
-        defaultExtension: 'js',
-        meta: {
-          './*.js': {
-            loader: 'systemjs-angular-loader.js'
-          }
-        }
-      },
-      rxjs: {
-        defaultExtension: 'js'
-      }
+      'src': {main: 'main', defaultExtension: 'js'},
+      '@angular/core': {main: 'core.umd.min.js'},
+      '@angular/common': {main: 'common.umd.min.js'},
+      '@angular/compiler': {main: 'compiler.umd.min.js'},
+      '@angular/forms': {main: 'forms.umd.min.js'},
+      '@angular/http': {main: 'http.umd.min.js'},
+      '@angular/platform-browser': {main: 'platform-browser.umd.min.js'},
+      '@angular/platform-browser-dynamic': {main:'platform-browser-dynamic.umd.min.js'},
+      'rxjs': {defaultExtension: 'js'}
     }
   });
 })(this);
